7565d27faea765226c510789f1dc9f9116b2a4d1
mustafa <mustafa.rpzyu20@sinhgad.edu>
2024-02-15 16:15:35 +0530

    Refactor code to improve performance and readability

--- number.py
+++ number.py
@@ -1,8 +1,9 @@
-from math import sqrt , floor
+from math import sqrt, floor
 from math import *
+
 # the above line is used to import the sqrt and floor functions from the math module
-# this is the math module in that we are importing the sqrt and floor functions  from the math module 
-# this module is used to perform the mathematical operations in python 
+# this is the math module in that we are importing the sqrt and floor functions  from the math module
+# this module is used to perform the mathematical operations in python
 # working with numbers in python  :
 # int data type : int data type is used to store the integer data in the variable
 a = 5
@@ -37,14 +38,14 @@ i = g - h
 print(i)
 
 # output : 5
-    
+
 # multiplication of two numbers in python  : the multiplication of two numbers is done by using the star operator
 
 j = 5
 k = 6
 l = j * k
 print(l)
-    
+
 # output : 30
 
 # division of two numbers in python  : the division of two numbers is done by using the slash operator
@@ -66,26 +67,26 @@ print(r)
 
 # power of two numbers in python  : the power of two numbers is done by using the double star operator
 s = 2
-print(pow(s,2))
-print(pow(s,3))
+print(pow(s, 2))
+print(pow(s, 3))
 
-# to convert number into strings 
-a =5
+# to convert number into strings
+a = 5
 print(str(a))
 
-# output : 5    
+# output : 5
 
 # the abs function is used to find the absolute value of the number
 t = -5
-print(abs(t)) 
+print(abs(t))
 
 #  output : 5
 
-print(3*2+5)
+print(3 * 2 + 5)
 
-# output : 11 
+# output : 11
 
-print(3*(2+5))
+print(3 * (2 + 5))
 
 # output : 21
 
@@ -100,11 +101,11 @@ print(sqrt(v))
 
 # output : 5.0
 
-# the max function is used to find the maximum number from the given numbers 
+# the max function is used to find the maximum number from the given numbers
 x = 20
 y = 30
 z = 40
-print( " the max value is : " + str(max(x,y,z)))
+print(" the max value is : " + str(max(x, y, z)))
 
 # output : the max value is 40
 
@@ -112,7 +113,7 @@ print( " the max value is : " + str(max(x,y,z)))
 a = 20
 b = 30
 c = 40
-print( " the min value is " + str(min(a,b,c)))
+print(" the min value is " + str(min(a, b, c)))
 
 # output : the min value is 20
 
@@ -126,19 +127,87 @@ print("The sum value is: " + str(sum([a, b])))
 # output : the sum value is 50
 
 # the floor function is used to find the floor value of the given number
-# floor number is the largest integer that is less than or equal to the number 
+# floor number is the largest integer that is less than or equal to the number
 a = 5.5
-print( " the floor value is " + str(floor(a)))
+print(" the floor value is " + str(floor(a)))
 
 # output : the floor value is 5
 
-# ceil is used to find the ceiling value of the given number 
+# ceil is used to find the ceiling value of the given number
 # the ceiling number is the smallest integer that is greater than or equal to the number
 a = 5.5
-print( " the ceil value is " + str(ceil(a)))
+print(" the ceil value is " + str(ceil(a)))
 
 # output : the ceil value is 6
-# math function list in python 
+
+# round function is used to round off the number
+a = 5.5
+print(" the round value is " + str(round(a)))
+
+# output : the round value is 6
+
+# the pi function is used to find the value of pi
+print(" the value of pi is : " + str(pi))
+
+# output : the value of pi is 3.141592653589793
+
+# the e function is used to find the value of e
+print(" the value of e is : " + str(e))
+
+# output : the value of e is 2.718281828459045
+
+# the log function is used to find the natural logarithm of the number
+a = 10
+print(" the natural logarithm of the number is : " + str(log(a)))
+
+# output : the natural logarithm of the number is : 2.302585092994046
+
+# the log10 function is used to find the base 10 logarithm of the number
+a = 10
+print(" the base 10 logarithm of the number is : " + str(log10(a)))
+
+# output : the base 10 logarithm of the number is : 1.0
+
+# the exp function is used to find the exponential value of the number
+a = 5
+print(" the exponential value of the number is : " + str(exp(a)))
+
+# output : the exponential value of the number is : 148.4131591025766
+
+# the sin function is used to find the sine value of the number
+a = 90
+print(" the sine value of the number is : " + str(sin(a)))
+
+# output : the sine value of the number is : 0.8939966636005579
+
+# the cos function is used to find the cosine value of the number
+a = 90
+print(" the cosine value of the number is : " + str(cos(a)))
+
+# output : the cosine value of the number is : -0.4480736161291701
+
+# the tan function is used to find the tangent value of the number
+a = 90
+print(" the tangent value of the number is : " + str(tan(a)))
+
+# output : the tangent value of the number is : -1.995200412208242
+
+# the asin function is used to find the arc sine value of the number
+a = 0.5
+print(" the arc sine value of the number is : " + str(asin(a)))
+
+# output : the arc sine value of the number is : 0.5235987755982989
+
+# the acos function is used to find the arc cosine value of the number
+a = 0.5
+
+
+print(" the arc cosine value of the number is : " + str(acos(a)))
+
+# output : the arc cosine value of the number is : 1.0471975511965979
+
+
+# math function list in python
 # abs() : this function is used to find the absolute value of the number
 # pow() : this function is used to find the power of the number
 # sqrt() : this function is used to find the square root of the number
@@ -148,44 +217,60 @@ print( " the ceil value is " + str(ceil(a)))
 # floor() : this function is used to find the floor value of the given number
 # ceil() : this function is used to find the ceiling value of the given number
 # round() : this function is used to round off the number
+# pi() : this function is used to find the value of pi
+# e() : this function is used to find the value of e
+# log() : this function is used to find the natural logarithm of the number
+# log10() : this function is used to find the base 10 logarithm of the number
+# exp() : this function is used to find the exponential value of the number
+# sin() : this function is used to find the sine value of the number
+# cos() : this function is used to find the cosine value of the number
+# tan() : this function is used to find the tangent value of the number
+# asin() : this function is used to find the arc sine value of the number
+# acos() : this function is used to find the arc cosine value of the number
+# atan() : this function is used to find the arc tangent value of the number
+# count() : this function is used to count the number of elements in the given list
+# factorial() : this function is used to find the factorial of the number
+# gcd() : this function is used to find the greatest common divisor of the given numbers
+# isfinite() : this function is used to check whether the given number is finite or not
+
 # the math module is used to perform the mathematical operations in python
 
-# input in the python That makes the input that takes input from the user 
+# input in the python That makes the input that takes input from the user
 name = input("enter the name : ")
 # varibale name = input (" string ")
 print(" hello my name is " + name + " !!!")
 
-# output : 
+# output :
 # enter the name : siva
 # hello my name is siva !!!
 
 
 age = input("enter the age : ")
-print (" my age is " + age + " years ")
+print(" my age is " + age + " years ")
 
 # output :
 # enter the age : 25
 # my age is 25 years
 
 
-# simple calculator in python 
-num1= int(input("enter the first number :"))
+# simple calculator in python
+num1 = int(input("enter the first number :"))
 num2 = int(input("enter the second number :"))
-print ("The sum of the given numbers is : "+ str(num1+num2))
+print("The sum of the given numbers is : " + str(num1 + num2))
 
-# output : 
+# output :
 # enter the first number :5
 # enter the second number :6
 # The sum of the given numbers is : 11
 
 
-# for a float number 
-num1= float(input("enter the first number :"))
+# for a float number
+num1 = float(input("enter the first number :"))
 num2 = float(input("enter the second number :"))
-result= num1+num2
-print(" the sum of the number is  :"+ str(result))
+result = num1 + num2
+print(" the sum of the number is  :" + str(result))
 
-#output :
+# output :
 # enter the first number :5.5
 # enter the second number :6.5
 # the sum of the number is  :12.0
@@ -194,15 +279,16 @@ print(" the sum of the number is  :"+ str(result))
 # one is float and another is int
 num3 = int(input("enter the first number :"))
 num4 = float(input("enter the second number :"))
-result = int(num3)+float(num4)
-print(" the sum of the number is  :"+ str(result))
+result = int(num3) + float(num4)
+print(" the sum of the number is  :" + str(result))
 
-# output : 
+# output :
 # enter the first number :5
 # enter the second number :6.5
 #     the sum of the number is  :11.5
 
 # the result will print the float output bcoz the float number is added to the int number
+
 
 
 
