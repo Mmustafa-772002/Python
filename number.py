8c84d7bcfb4f9a9dc51572d1d77d276a55cd3b6f
mustafa <mustafa.rpzyu20@sinhgad.edu>
2024-02-15 10:02:22 +0530

    added number.py file

---
+++ number.py
@@ -0,0 +1,75 @@
+# working with numbers in python  :
+# int data type : int data type is used to store the integer data in the variable
+a = 5
+print(a)
+
+# output : 5
+
+# float data type : float data type is used to store the float data in the variable
+b = 5.5
+print(b)
+
+# output : 5.5
+
+# negetive numbers in python  : the negetive numbers are also used in python
+c = -5
+print(c)
+
+# output : -5
+
+# addition of two numbers in python  : the addition of two numbers is done by using the plus operator
+d = 5
+e = 6
+f = d + e
+print(f)
+
+# output : 11
+
+# subtraction of two numbers in python  : the subtraction of two numbers is done by using the minus operator
+g = 10
+h = 5
+i = g - h
+print(i)
+
+# output : 5
+    
+# multiplication of two numbers in python  : the multiplication of two numbers is done by using the star operator
+
+j = 5
+k = 6
+l = j * k
+print(l)
+    
+# output : 30
+
+# division of two numbers in python  : the division of two numbers is done by using the slash operator
+m = 10
+n = 5
+o = m / n
+print(o)
+
+# output : 2.0
+
+# modulus of two numbers in python  : the modulus of two numbers is done by using the percentage operator
+
+p = 10
+q = 3
+r = p % q
+print(r)
+
+# output : 1
+
+# power of two numbers in python  : the power of two numbers is done by using the double star operator
+s = 2
+print(pow(s,2))
+print(pow(s,3))
+
+# to convert number into strings 
+a =5
+print(str(a))
+
+# output : 5
+
+# the abs function is used to find the absolute value of the number
+t = -5
+print(abs(t))     
