8b7d1ba23aa8e406f8eff8936b05d662e278c3fc
mustafa <mustafa.rpzyu20@sinhgad.edu>
2024-02-15 16:17:43 +0530

    Fix formatting and comments in hello world.py and string.py

--- hello world.py
+++ hello world.py
@@ -1,26 +1,26 @@
-print("hello world ") # output : hello world
+print("hello world ")  # output : hello world
 print("there is man  ")  # output : there is man
-print("he was 35 yaers of old  ") # output : he was 35 yaers of old
-print("he really like to play cricket ") # output : he really like to play cricket
-# this is commet in the python 
+print("he was 35 yaers of old  ")  # output : he was 35 yaers of old
+print("he really like to play cricket ")  # output : he really like to play cricket
+# this is commet in the python
 # variable : varible is the data containeter in that we can store the data statically or dynamically
 # static variable : static variable is the variable in that we can store the data statically
 # dynamic variable : dynamic variable is the variable in that we can store the data dynamically
 # example of static variable
 charachter_name = "mike tison"
 print("my name is " + charachter_name)
-# this is also called as concatenation 
-print(charachter_name.upper()) # output : MIKE
-# this is also called as upper case it is used to convert the string into upper case 
-print(charachter_name.lower()) # output : mike
+# this is also called as concatenation
+print(charachter_name.upper())  # output : MIKE
+# this is also called as upper case it is used to convert the string into upper case
+print(charachter_name.lower())  # output : mike
 # this is also called as lower case it is used to convert the string into lower case
-print(charachter_name.isupper()) # output : False
+print(charachter_name.isupper())  # output : False
 # this is also called as isupper it is used to check the string is in upper case or not
-print(charachter_name.islower()) # output : False
+print(charachter_name.islower())  # output : False
 # this is also called as islower it is used to check the string is in lower case or not
-print(charachter_name.upper().isupper()) # output : True
+print(charachter_name.upper().isupper())  # output : True
 # this is also called as upper and isupper it is used to convert the string into upper case and check the string is in upper case or not
-print(len(charachter_name)) # output : 10
+print(len(charachter_name))  # output : 10
 # this is also called as len it is used to find the length of the string
 
 # output : my name is mike
@@ -37,10 +37,10 @@ print("my age is " + my_age)
 # 3. float variable
 # 4. boolean type
 # example of string variable
-name = "mike"   
+name = "mike"
 print(name)
 
-#output : mike
+# output : mike
 
 # example of integer variable
 age = 35
@@ -58,7 +58,7 @@ print(height)
 male = True
 print(male)
 
-#output : True
+# output : True
 
 
 # string variable : string variable is the variable in that we can store the string data
@@ -67,14 +67,13 @@ print(male)
 # boolean type : boolean type is the type in that we can store the true or false value
 
 # \n is used for new line
-print("hello \nworld")  # output : hello 
-                        #          world 
+print("hello \nworld")  # output : hello
+#          world
 # \t is used for tab
 print("hello \tworld")  # output : hello     world
-# \" is used for double quote 
-print("hello \"world\"") # output : hello "world"
+# \" is used for double quote
+print('hello "world"')  # output : hello "world"
 # \' is used for single quote
-print("hello \'world\'") # output : hello 'world'
+print("hello 'world'")  # output : hello 'world'
 # \\ is used for backslash
-print("hello \\world")   # output : hello \world
-
+print("hello \\world")  # output : hello \world
--- string.py
+++ string.py
@@ -1,16 +1,16 @@
-# to access the character of a string, use the square brackets for slicing along with the index or indices to obtain your character or substring. 
-#For example −
+# to access the character of a string, use the square brackets for slicing along with the index or indices to obtain your character or substring.
+# For example −
 word = " this is pyhton programming language"
 print(word[6])
 
-#output : i
+# output : i
 
 # index function is used to find the index of the character in the string
 print(word.index("p"))
 print(word.index(" is"))
-# output : 9 
+# output : 9
 
-# replace function is used to replace the word in the string 
+# replace function is used to replace the word in the string
 
 replace = word.replace("pyhton", "java")
 print(replace)
