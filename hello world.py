cab49abdbf9599e2886572dc218307e65de5bfa2
mustafa <mustafa.rpzyu20@sinhgad.edu>
2024-02-15 09:30:14 +0530

    string

---
+++ hello world.py
@@ -0,0 +1,80 @@
+print("hello world ") # output : hello world
+print("there is man  ")  # output : there is man
+print("he was 35 yaers of old  ") # output : he was 35 yaers of old
+print("he really like to play cricket ") # output : he really like to play cricket
+# this is commet in the python 
+# variable : varible is the data containeter in that we can store the data statically or dynamically
+# static variable : static variable is the variable in that we can store the data statically
+# dynamic variable : dynamic variable is the variable in that we can store the data dynamically
+# example of static variable
+charachter_name = "mike tison"
+print("my name is " + charachter_name)
+# this is also called as concatenation 
+print(charachter_name.upper()) # output : MIKE
+# this is also called as upper case it is used to convert the string into upper case 
+print(charachter_name.lower()) # output : mike
+# this is also called as lower case it is used to convert the string into lower case
+print(charachter_name.isupper()) # output : False
+# this is also called as isupper it is used to check the string is in upper case or not
+print(charachter_name.islower()) # output : False
+# this is also called as islower it is used to check the string is in lower case or not
+print(charachter_name.upper().isupper()) # output : True
+# this is also called as upper and isupper it is used to convert the string into upper case and check the string is in upper case or not
+print(len(charachter_name)) # output : 10
+# this is also called as len it is used to find the length of the string
+
+# output : my name is mike
+
+# example of dynamic variable
+my_age = input("enter your age : ")
+print("my age is " + my_age)
+
+# output : my age is 35
+
+# varible types : there are 3 types of variable in python
+# 1. string variable
+# 2. integer variable
+# 3. float variable
+# 4. boolean type
+# example of string variable
+name = "mike"   
+print(name)
+
+#output : mike
+
+# example of integer variable
+age = 35
+print(age)
+
+# output : 35
+
+# example of float variable
+height = 5.8
+print(height)
+
+# output : 5.8
+
+# example of boolean
+male = True
+print(male)
+
+#output : True
+
+
+# string variable : string variable is the variable in that we can store the string data
+# integer variable : integer variable is the variable in that we can store the integer data
+# float variable : float variable is the variable in that we can store the float data
+# boolean type : boolean type is the type in that we can store the true or false value
+
+# \n is used for new line
+print("hello \nworld")  # output : hello 
+                        #          world 
+# \t is used for tab
+print("hello \tworld")  # output : hello     world
+# \" is used for double quote 
+print("hello \"world\"") # output : hello "world"
+# \' is used for single quote
+print("hello \'world\'") # output : hello 'world'
+# \\ is used for backslash
+print("hello \\world")   # output : hello \world
+
